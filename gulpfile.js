8a580d43b4a151c5e81297f5f5b3139973e646de
Alex McCabe <hello@alexmccabe.me>
2014-07-07 15:16:21 +0100

    Gulpfile: updated sources object

--- gulpfile.js
+++ gulpfile.js
@@ -52,16 +52,16 @@ gulp.task('styles', function() {
 			displayError(err);
 		})
 		.pipe(plugins.autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-		.pipe(gulp.dest(sources.dest))
+		.pipe(gulp.dest(sources.sass.dest))
 
 		// Minify the CSS file
 		.pipe(plugins.rename({suffix: '.min'}))
 		.pipe(plugins.minifyCss())
-		.pipe(gulp.dest(sources.dest))
+		.pipe(gulp.dest(sources.sass.dest))
 
 		.pipe(plugins.notify({message: 'Styles task complete'}));
 });
 
 gulp.task('default', function() {
-
+	console.log(sources.sass.dest);
 });
