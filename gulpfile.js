fffa7970d914689b0c365a6f981cb23c43ec6c8c
Alex McCabe <hello@alexmccabe.me>
2014-07-07 11:48:16 +0100

    Added dev area

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
 var gulp = require('gulp');
+var gulpLoadPlugins = require('gulp-load-plugins');
+var plugins = gulpLoadPlugins();
 
 gulp.task('default', function() {
 
+});
+
+gulp.task('greet', function() {
+	console.log('Hello world');
 });
